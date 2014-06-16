d3a48f52c67dfadcd3f2fad4f60b2af538153f5e
Gabriel Gomes <gomes@path.berkeley.edu>
2014-06-16 12:16:46 -0700

    data and fds

--- src/test_load_fwy.m
+++ src/test_load_fwy.m
@@ -1,12 +1,15 @@
 clear
 close all
 
-root = fileparts(fileparts(mfilename('fullpath')));
-addpath(fullfile(root,'src','def'));
+% root = fileparts(fileparts(mfilename('fullpath')));
+% addpath(fullfile(root,'src','def'));
+% 
+% % test load scenario
+% se = ScenarioPtr;
+% se.load(fullfile(root,'cfg','210W_v8.xml'));
 
-% test load scenario
-se = ScenarioPtr;
-se.load(fullfile(root,'cfg','210W_v8.xml'));
+load aaa
+
 fwy = Freeway(se);
 
 % test get link id
@@ -18,6 +21,13 @@ S = fwy.get_sensor_with_vds(717624);
 % test get sensor with link id
 S = fwy.get_sensor_with_linkid(196);
 
+% test load loop health for days
+fwy.load_loop_health_for_days(fullfile(root,'generated','data'),[735632 234532 735634 735637])
+
+% test load data
+fwy.load_5min_data_for_days(fullfile(root,'generated','data'),[735632 234532 735634 735637])
+
+load aaa
 
 
 
